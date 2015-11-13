cf789faa17f75ad71e3e64d93c3d80998458b6c5
Dariusz Kobylarz <darek.kobylarz@gmail.com>
2015-11-13 17:40:41 +0100

    SuperAdminLogin class reference removed

--- vitaladmin-auth-mod/src/main/groovy/ai/vital/adminauth/mod/VitalAdminAuthManager.groovy
+++ vitaladmin-auth-mod/src/main/groovy/ai/vital/adminauth/mod/VitalAdminAuthManager.groovy
@@ -1,42 +1,7 @@
 package ai.vital.adminauth.mod
 
-import groovy.lang.Closure;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.githubusercontent.defuse.passwordhash.PasswordHash;
-import org.vertx.groovy.core.eventbus.EventBus
-import org.vertx.groovy.platform.Verticle
-import org.vertx.groovy.core.eventbus.Message;
-import org.vertx.java.core.Future;
-
-import ai.vital.auth.mod.VitalAuthManager;
+import ai.vital.auth.mod.VitalAuthManager
 import ai.vital.auth.mod.VitalAuthManager.AuthAppBean
-import ai.vital.auth.queries.Queries;
-import ai.vital.domain.CredentialsLogin;
-import ai.vital.domain.Login;
-import ai.vital.domain.AdminLogin
-import ai.vital.domain.SuperAdminLogin
-import ai.vital.domain.UserSession;
-import ai.vital.service.admin.vertx.VitalServiceAdminMod;
-import ai.vital.service.admin.vertx.async.VitalServiceAdminAsyncClient;
-import ai.vital.service.vertx.VitalServiceMod;
-import ai.vital.service.vertx.async.VitalServiceAsyncClient;
-import ai.vital.service.vertx.binary.ResponseMessage;
-import ai.vital.vitalservice.VitalStatus;
-import ai.vital.vitalservice.admin.VitalServiceAdmin;
-import ai.vital.vitalservice.factory.VitalServiceFactory;
-import ai.vital.vitalservice.json.VitalServiceJSONMapper;
-import ai.vital.vitalservice.query.ResultList;
-import ai.vital.vitalservice.query.VitalSelectQuery
-import ai.vital.vitalsigns.VitalSigns;
-import ai.vital.vitalsigns.meta.GraphContext;
-import ai.vital.vitalsigns.model.GraphObject
-import ai.vital.vitalsigns.model.VitalApp
-import ai.vital.vitalsigns.model.VitalSegment
-import ai.vital.vitalsigns.model.property.URIProperty;
 
 
 
